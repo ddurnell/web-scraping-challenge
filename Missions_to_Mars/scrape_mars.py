8f6c54d1e1d120b314baf468879c653c9ee52648
ddurnell <davereally@gmail.com>
2020-03-09 12:40:58 -0600

    scraping py 1.0

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,7 +1,7 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
-import time
-
+import requests
+import pandas as pd
 
 def init_browser():
     # @NOTE: Replace the path with your actual path to the chromedriver
@@ -66,8 +66,86 @@ def scrape_mars_image(browser, url):
 
     # make it a real, not relative path
     featured_image_url = "https:" + path
-    result = {'mars_image':featured_image_url}
-    return result
+    return featured_image_url
+
+def scrape_mars_weather(browser, url):
+    browser.visit(url)
+    # sending get request and saving the response as response object 
+    r = requests.get(url = url) 
+  
+    # extracting the end result since the whole thing is javascript 
+    html = r.text
+    soup = bs(html)
+
+    # gather the tweets
+    tweets = soup.find_all("div", class_="js-tweet-text-container")
+
+    # extract the .jpg file path
+    for t in tweets:
+        p = t.contents[1]
+        text = p.contents[0]
+        if text.startswith('InSight'):
+            mars_weather = text
+            break
+    return mars_weather
+
+def scrape_mars_facts(browser, url):
+    # go to NASA and get the soup
+    browser.visit(url)
+    html = browser.html
+    soup = bs(html, 'lxml')
+
+    tables = pd.read_html(url)
+    fact_table = tables[0]
+    print(fact_table)
+
+    return fact_table.to_html()
+
+# Subfunction for scrape_mars_hemispheres
+# For each url, navigate to the enhanced tif, and return its link
+def get_enhanced_image(browser, url):
+#     print(url)
+    browser.visit(url)
+    html = browser.html
+    soup = bs(html, 'lxml')    
+    ds = soup.find_all('div', class_='downloads')
+    for d in ds:
+        link = d.a
+        h = link.get('href')
+#         print(h)
+    return h
+
+def scrape_mars_hemispheres(browser, url):
+    # go and get the soup
+    browser.visit(url)
+    html = browser.html
+    soup = bs(html, 'lxml')
+
+    # Extract the titles and the links
+    result = soup.find("div", class_="collapsible results")
+
+    next_dict = {} # the links to get to the enhanced images
+    results = result.find_all('div')
+    for r in results:
+        item = r.contents[1]
+        links = item.find_all('a')
+        for link in links:
+            title = link.find('h3').text
+            href = link['href']
+            href = "https://astrogeology.usgs.gov/" + href
+            next_dict[title] = href
+
+    # Go find the actual url for the enhanced image
+    # Loop through the dictionary
+    results_list = []
+    for key in next_dict:
+    #     print(key)
+    #     print(next_dict[key])
+        url = get_enhanced_image(browser, next_dict[key])
+        d = {"title": key, "img_url": url}
+        results_list.append(d)
+    return results_list
+
 
 def scrape():
     browser = init_browser()
@@ -81,6 +159,18 @@ def scrape():
     mars_image = scrape_mars_image(browser, 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars')
     mars_data['image'] = mars_image
     
+    # get mars image
+    mars_weather = scrape_mars_weather(browser, 'https://twitter.com/marswxreport?lang=en')
+    mars_data['weather'] = mars_weather
+
+    # get mars facts table
+    mars_facts = scrape_mars_facts(browser, 'https://space-facts.com/mars/')
+    mars_data['facts'] = mars_facts
+
+    # get mars hemispheres links
+    mars_hems = scrape_mars_hemispheres(browser, 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars')
+    mars_data['hemisphere_image_urls'] = mars_hems
+
     # Close the browser after scraping
     browser.quit()
 
